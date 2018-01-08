e4776c05a41ad0eeb8db657e21a0063c9bfd6f5e
greenmonking <greenmonking@outlook.com>
2018-01-08 20:41:49 +0800

    second

---
+++ scripts/edit.js
@@ -0,0 +1,132 @@
+$(function(){
+	
+	/**
+	 * 动画切换
+	 */
+	var mySwiper = new Swiper('.swiper-container', {
+		direction: 'vertical',
+//		effect : 'cube',
+//	    loop: true,
+		onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+//			wiperAnimateCache(swiper); //隐藏动画元素 
+	        swiperAnimate(swiper); //初始化完成开始动画
+	    }, 
+	    onSlideChangeEnd: function(swiper){ 
+	        swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+	    } 
+	})
+	
+	
+	/**
+	 * 删除当前页
+	 */
+	$(document).on('click','.deletePage',function(ev){
+		var btnArray = ['取消', '确定'];
+        mui.confirm('确认删除当前页？', '信息提示', btnArray, function(e) {
+            if (e.index == 1) {
+            	//确定删除
+            	var pageIndex = $('.swiper-slide').index($('.swiper-slide-active'));
+				mySwiper.removeSlide(pageIndex);
+            } else {
+            	//取消操作
+//              alert(2)
+            }
+        })
+	})
+	
+	
+	/**
+	 * 增加页
+	 */
+	$(document).on('click','.addPage',function(ev){
+		var pageIndex = $('.swiper-slide').index($('.swiper-slide-active'));
+
+		
+		$('.swiper-slide-active').after(` <div class="swiper-slide">
+				    	<img src="images/img2.jpg" class="img1"/>
+				    </div>`)
+		//重新计算slider数量
+		mySwiper.updateSlidesSize();
+		
+		//swiper切换到指定slider
+		mySwiper.slideTo(pageIndex + 1, 50, false);
+		
+		//swiper2.0里面提供的方法
+		//var newSlide = mySwiper.insertSlideAfter(pageIndex,'<p>这是一个新的slide</p>','swiper-slide ','div');
+	})
+	
+	/**
+	 *点击编辑文本
+	 */
+	$(document).on("click", ".editText",function(event) {
+		var option = new Object();
+		var randomId = parseInt(Math.random()*(1-10000)+10000);
+		option.defaultVal = event.target.innerHTML;
+		option.charLen = event.target.innerHTML.length;
+		option.id = randomId;
+		event.target.id = randomId;
+		
+		fnCreatePrompt(option)
+		
+		/**
+		 * 实时检测文本框内的字数，并展示出来
+		 */
+		$('#editBox textarea').on('input propertychange',function(){
+			$('.valueLen').text($(this)[0].value.length)
+		});
+		
+	});
+	
+	/**
+	 * 点击更换图片
+	 */
+	$(document).on("click", ".editImg",function(event) {
+		
+		console.log(event.target)
+	});
+	
+	/**
+	 *取消编辑文本
+	 */
+	$(document).on("click", "#cancel",function(event) {
+		$('.createPrompt').html('')
+		
+	});
+	
+	/**
+	 *确认编辑文本
+	 */
+	$(document).on("click", "#ensure",function(event) {
+		var dataId = event.target.getAttribute('data-id') ;
+		$('#' + dataId).text($('textarea').val())
+		$('#' + dataId).removeAttr('id');
+		$('.createPrompt').html('')
+	});
+	
+	/**
+	 * 自定义文本修改框
+	 */
+	fnCreatePrompt = function(options){
+		var theDomData = $.extend(true, {
+			id : '',
+			defaultVal :  '' ,
+			charLen:''
+		}, options);
+	 	var createPrompt = document.querySelectorAll(".createPrompt");
+		var domHtml =  `<div id="editBox">
+							<textarea name="" rows="" cols="" placeholder="">`+ theDomData.defaultVal +`</textarea>
+							<p><span class="valueLen">`+ theDomData.charLen +`</span>/200</p>
+							<div class="btn">
+								<button id="cancel" type="button" class="mui-btn mui-btn-outlined">取消</button>
+								<button id="ensure" data-id ="`+ theDomData.id +`" type="button" class="mui-btn mui-btn-outlined">确认</button>
+							</div>
+						</div>
+						<div id="editBoxZoom"></div>`
+							
+		$('.createPrompt').append(domHtml)
+		
+	}
+	
+	
+	
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
 $(function(){
+	
+	mui.init();
+	
 	/**
 	 * 动画切换
 	 */
 	var mySwiper = new Swiper('.swiper-container', {
 		direction: 'vertical',
 //		effect : 'cube',
-	    loop: true,
+//	    loop: true,
 		onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
 //			wiperAnimateCache(swiper); //隐藏动画元素 
 	        swiperAnimate(swiper); //初始化完成开始动画
@@ -20,69 +23,164 @@ $(function(){
 	 *点击编辑按钮
 	 */
 	$(document).on("click", "#edit",function(event) {
-		$('#edit').hide();
-		$('#share').hide();
-		$('.editableText').addClass('editText');
+		window.location.href='./edit.html'; 
+//		$('#edit').hide();
+//		$('#share').hide();
+//		$('.editableText').addClass('editText');
+//		$('.editableImg').addClass('editImg');
+	});
+	
+
+	//--创建页面监听，等待微信端页面加载完毕 触发音频播放
+	document.addEventListener('DOMContentLoaded', function () {
+	    function audioAutoPlay() {
+	        var media = document.getElementById('media');
+	            media.play();
+	        document.addEventListener("WeixinJSBridgeReady", function () {
+	            media.play();
+	        }, false);
+	    }
+	    audioAutoPlay();
+	});
+	//--创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
+	document.addEventListener('touchstart', function () {
+	    function audioAutoPlay() {
+	        var media = document.getElementById('media');
+	            media.play();
+	    }
+	    audioAutoPlay();
 	});
 	
 	/**
-	 *点击编辑文本
+	 * 控制音乐的播放暂停
 	 */
-	$(document).on("click", ".editText",function(event) {
-		var option = new Object();
-		var randomId = parseInt(Math.random()*(1-10000)+10000);
-		option.defaultVal = event.target.innerHTML;
-		option.charLen = event.target.innerHTML.length;
-		option.id = randomId;
-		event.target.id = randomId;
-		
-		fnCreatePrompt(option)
-			
-		
+	var x = document.getElementById("media"); 
+//	$('html').on('touchstart',function(){
+//		x .play();
+//	});
+	$(function(){
+	    $("#audio_btn").click(function(){
+	        $(this).toggleClass("rotate"); //控制音乐图标 自转或暂停
+	        
+	        //控制背景音乐 播放或暂停            
+	        if($(this).hasClass("rotate")){
+	            x.play();
+	        }else{
+	            x.pause();
+	        }
+	    })
 	});
 	
-	
-	/**
-	 *取消编辑文本
-	 */
-	$(document).on("click", "#cancel",function(event) {
-		$('.createPrompt').html('')
-		
-	});
-	
-	/**
-	 *确认编辑文本
-	 */
-	$(document).on("click", "#ensure",function(event) {
-		var dataId = event.target.getAttribute('data-id') ;
-		$('#' + dataId).text($('textarea').val())
-		$('#' + dataId).removeAttr('id');
-		$('.createPrompt').html('')
-	});
-	
-	/**
-	 * 自定义文本修改框
-	 */
-	fnCreatePrompt = function(options){
-		var theDomData = $.extend(true, {
-			id : '',
-			defaultVal :  '' ,
-			charLen:''
-		}, options);
-	 	var createPrompt = document.querySelectorAll(".createPrompt");
-		var domHtml =  `<div id="editBox">
-							<textarea name="" rows="" cols="" placeholder="">`+ theDomData.defaultVal +`</textarea>
-							<p><span class="valueLen">`+ theDomData.charLen +`</span>/200</p>
-							<div class="btn">
-								<button id="cancel" type="button" class="mui-btn mui-btn-outlined">取消</button>
-								<button id="ensure" data-id ="`+ theDomData.id +`" type="button" class="mui-btn mui-btn-outlined">确认</button>
-							</div>
-						</div>
-						<div id="editBoxZoom"></div>`
-							
-		$('.createPrompt').append(domHtml)
-		
-	}
-	
-	
+//	function plusReady(){  
+//      // 弹出系统选择按钮框  
+//      mui("body").on("tap",".imageup",function(){  
+//          page.imgUp();  
+//      })  
+//                
+//	}  
+//             
+//  var page=null;  
+//  page={  
+//      imgUp:function(){  
+//          var m=this;  
+//          plus.nativeUI.actionSheet({cancel:"取消",buttons:[  
+//              {title:"拍照"},  
+//              {title:"从相册中选择"}  
+//          ]}, function(e){//1 是拍照  2 从相册中选择  
+//              switch(e.index){  
+//                  case 1:clickCamera();break;  
+//                  case 2:clickGallery();break;  
+//              }  
+//          });  
+//      }  
+//      //摄像头  
+//  }  
+//            
+//            
+//   //发送照片  
+//    
+//  function clickGallery() {  
+//      plus.gallery.pick(function(path) {  
+//          plus.zip.compressImage({  
+//              src: path,  
+//              dst: "_doc/chat/gallery/" + path,  
+//              quality: 20,  
+//              overwrite: true  
+//          }, function(e) {  
+//              var task = plus.uploader.createUpload(server + "upload/chat", {  
+//                  method: "post"  
+//              }, function(t, sta) {  
+//                  console.log(JSON.stringify(t))  
+//                  if(sta == 200) {  
+//                      var msg = t.responseText;  
+//                      var oImg = JSON.parse(msg);  
+//                      var imgUrl = oImg.urls;  
+//                      var re = new RegExp("\\\\", "g");  
+//                      imgUrl = imgUrl.replace(re, "/");  
+//                      uploadMsg(2, imgUrl);  
+//                  }  
+//              });  
+//              task.addFile(e.target, {});  
+//              task.start();  
+//          }, function(err) {  
+//              console.error("压缩失败：" + err.message);  
+//          });  
+//
+//      }, function(err) {});  
+//  };  
+//    
+//    
+//  // 拍照  
+//    
+//  function clickCamera() {  
+//      var cmr = plus.camera.getCamera();  
+//      var res = cmr.supportedImageResolutions[0];  
+//      var fmt = cmr.supportedImageFormats[0];  
+//      cmr.captureImage(function(path) {  
+//          //plus.io.resolveLocalFileSystemURL(path, function(entry) {  
+//          plus.io.resolveLocalFileSystemURL(path, function(entry) {  
+//              var localUrl = entry.toLocalURL();  
+//              plus.zip.compressImage({  
+//                  src: localUrl,  
+//                  dst: "_doc/chat/camera/" + localUrl,  
+//                  quality: 20,  
+//                  overwrite: true  
+//              }, function(e) {  
+//                  var task = plus.uploader.createUpload(server + "upload/chat", {  
+//                      method: "post"  
+//                  }, function(t, sta) {  
+//                      if(sta == 200) {  
+//                          var msg = t.responseText;  
+//                          var oImg = JSON.parse(msg);  
+//                          var imgUrl = oImg.urls;  
+//                          var re = new RegExp("\\\\", "g");  
+//                          imgUrl = imgUrl.replace(re, "/");  
+//                          console.log(imgUrl);  
+//                          uploadMsg(2, imgUrl);  
+//                      }  
+//                  });  
+//                  task.addFile(e.target, {});  
+//                  task.start();  
+//              }, function(err) {  
+//                  console.log("压缩失败：  " + err.message);  
+//              });  
+//          });  
+//      }, function(err) {  
+//          console.error("拍照失败：" + err.message);  
+//      }, {  
+//          index: 1  
+//      });  
+//  };  
+//
+//
+//            
+              
+//          if(window.plus){  
+//plusReady();
+//                
+//          }else{  
+//              document.addEventListener("plusready",plusReady,false);  
+//          }  
+            
+            
 });
